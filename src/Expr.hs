a8dc99c483bf283ff6d1bbaa936741f14c976741
Michael Bottini <mbottini1@gmail.com>
2022-04-28 15:42:35 -0700

    Start Expr module

---
+++ src/Expr.hs
@@ -0,0 +1,10 @@
+-- An Expr is either an Atom or a List of Expressions.
+-- For now, an Atom is either a keyword or an integer.
+-- After that, we'll hopefully add closures and a numerical tower.
+
+module Expr where
+
+data Atom = Number Int | Keyword String
+
+data Expr = Atom Atom | List [Expr]
+
