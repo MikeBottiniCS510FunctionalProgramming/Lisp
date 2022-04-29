a3dfaba808dab3b2bcf414a3541f2e750eb65fc3
Michael Bottini <mbottini1@gmail.com>
2022-04-29 15:42:13 -0700

    Add parseAndEval function

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Expr
+import Parser
+import Builtins
+
+import Text.ParserCombinators.Parsec
+import qualified Data.Map.Strict as Map
+
+parseAndEval :: String -> Either ParseError Expr
+parseAndEval s = eval Map.empty <$> parse expr "" s
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
