49ac19ac8a47c8080e9b96a107c06905dfab75b1
Michael Bottini <mbottini1@gmail.com>
2022-05-04 21:28:46 -0700

    Add more functions to Prelude

--- src/Main.hs
+++ src/Main.hs
@@ -16,13 +16,15 @@ prelude1 = fromRight (error "eval error!") . parseAndEval Map.empty <$> Map.from
     ("cons", "(lambda (x xs) (cons x xs))"),
     ("car", "(lambda (xs) (car xs))"),
     ("cdr", "(lambda (xs) (cdr xs))"),
-    ("foldl", "(label foldl (f acc xs) (if (atom xs) acc (foldl f (f acc (car xs)) (cdr xs))))")]
+    ("foldl", "(label foldl (f acc xs) (if (atom xs) acc (foldl f (f acc (car xs)) (cdr xs))))"),
+    ("flip", "(lambda (f) (lambda (x y) (f y x)))")]
 
 prelude2 = Map.union prelude1 $ fromRight (error "eval error!") .  parseAndEval prelude1 <$> Map.fromList [
-    ("reverse", "(lambda (xs) (foldl (lambda (result x) (cons x result)) () xs))")]
+    ("reverse", "(lambda (xs) (foldl (flip cons) () xs))")]
     
 prelude3 = Map.union prelude2 $ fromRight (error "eval error!") . parseAndEval prelude2 <$> Map.fromList [
-    ("map", "(lambda (f xs) (reverse (foldl (lambda (acc x) (cons (f x) acc)) () xs)))")]
+    ("map", "(lambda (f xs) (reverse (foldl (lambda (acc x) (cons (f x) acc)) () xs)))"),
+    ("concat", "(lambda (xs ys) (foldl (flip cons) ys (reverse xs)))")]
 
 
 parseAndEvalPrelude :: String -> Either ParseError Expr
